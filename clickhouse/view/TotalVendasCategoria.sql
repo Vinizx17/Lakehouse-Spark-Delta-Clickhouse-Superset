f71798b4f41345b6c641542c6d99d988fae922cd
vinizx <86113102+Vinizx17@users.noreply.github.com>
2025-07-24 13:14:50 -0300

    Update TotalVendasCategoria.sql

--- clickhouse/view/TotalVendasCategoria.sql
+++ clickhouse/view/TotalVendasCategoria.sql
@@ -2,7 +2,27 @@ SELECT
   p.Categoria,
   SUM(f.Total_Vendas) AS Total_Vendas,
   SUM(f.Quantidade) AS Total_Quantidade
-FROM fato_pedidos f
-JOIN dim_produto p ON f.Product_ID = p.Product_ID
+FROM
+(
+  SELECT
+    ID_Pedido,
+    argMax(Product_ID, process_timestamp) AS Product_ID,
+    argMax(ID_Cliente, process_timestamp) AS ID_Cliente,
+    argMax(Total_Vendas, process_timestamp) AS Total_Vendas,
+    argMax(Quantidade, process_timestamp) AS Quantidade
+  FROM fato_pedidos
+  GROUP BY ID_Pedido
+) AS f
+JOIN
+(
+  SELECT
+    Product_ID,
+    argMax(Categoria, process_timestamp) AS Categoria,
+    argMax(SubCategoria, process_timestamp) AS SubCategoria
+  FROM dim_produto
+  GROUP BY Product_ID
+) AS p
+ON f.Product_ID = p.Product_ID
 GROUP BY p.Categoria
 ORDER BY Total_Vendas DESC
+
