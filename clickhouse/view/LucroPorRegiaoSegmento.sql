3ef03047538989d3a404107deb6d2cc7d4cf5231
vinizx <86113102+Vinizx17@users.noreply.github.com>
2025-07-24 13:14:31 -0300

    Update LucroPorRegiaoSegmento.sql

--- clickhouse/view/LucroPorRegiaoSegmento.sql
+++ clickhouse/view/LucroPorRegiaoSegmento.sql
@@ -2,7 +2,24 @@ SELECT
   c.Regiao,
   c.Segmento,
   SUM(f.Lucro) AS Total_Lucro
-FROM fato_pedidos f
-JOIN dim_cliente c ON f.ID_Cliente = c.ID_Cliente
+FROM
+(
+  SELECT
+    ID_Pedido,
+    argMax(ID_Cliente, process_timestamp) AS ID_Cliente,
+    argMax(Lucro, process_timestamp) AS Lucro
+  FROM fato_pedidos
+  GROUP BY ID_Pedido
+) AS f
+JOIN
+(
+  SELECT
+    ID_Cliente,
+    argMax(Regiao, process_timestamp) AS Regiao,
+    argMax(Segmento, process_timestamp) AS Segmento
+  FROM dim_cliente
+  GROUP BY ID_Cliente
+) AS c
+ON f.ID_Cliente = c.ID_Cliente
 GROUP BY c.Regiao, c.Segmento
-ORDER BY c.Regiao, c.Segmento
+ORDER BY c.Regiao, c.Segmento;
